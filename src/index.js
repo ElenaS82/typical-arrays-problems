e19cc3bdd0bfb5f9b6a4f66d89141b74b01267d1
ElenaS82 <sergeevaelenavl@gmail.com>
2021-08-17 22:41:43 +0300

    feat: add main script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0 ) {
+    return 0;
+   }
+   let arr = array.sort((a, b) => a - b);
+   return arr[0];
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0 ) {
+    return 0; 
+   }
+   let arr = array.sort((a, b) => a - b);
+   return arr.reverse()[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length == 0 || array.length == 0 ) {
+    return 0; 
+   }
+   let sum = 0;
+   for (let i = 0; i < array.length; i++) {
+     sum = sum + array[i];
+   }
+    return sum / array.length;
 }
